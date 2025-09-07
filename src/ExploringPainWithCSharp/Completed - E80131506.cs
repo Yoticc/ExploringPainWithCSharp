1ff746e27ce4df353912b31abf2df1ca9e8c68d9
Egor Bochkarev <yoticcitoy@gmail.com>
2025-09-07 15:12:58 +0300

    Add initial projects and two errors

---
+++ src/ExploringPainWithCSharp/Completed - E80131506.cs
@@ -0,0 +1,11 @@
+﻿[InternalClrError("COR_E_EXECUTIONENGINE: 0x80131506", ErrorStatus.Completed)]
+unsafe class E80131506
+{
+    static void Execute()
+    {
+        var @object = new object();
+        *(nint*)&@object = *(nint*)&@object + 0x10;
+
+        GC.Collect();
+    }
+}
---
+++ src/ExploringPainWithCSharp/InProgress - E80004005.cs
@@ -0,0 +1,8 @@
+﻿[InternalClrError("E_FAIL: 0x80004005", ErrorStatus.InProcess)]
+class E80004005
+{
+    static void Execute()
+    {
+        throw new AbandonedMutexException();
+    }
+}
